a88c9f29dee0b2c066a5e29c7b9d3cb7eb885f9b
Andrei Murariu <andrei.smurariu@proton.me>
2026-09-11 00:04:24 +0300

    chore: improve SEO for blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -37,9 +37,46 @@ export default async function BlogPost({ params }: PageProps) {
   const post = getPostBySlug(slug);
   if (!post) notFound();
 
+  const siteUrl = `https://${siteConfig.domain}`;
+  const pageUrl = `${siteUrl}/blog/${slug}`;
+  const ogImage = `${siteUrl}/opengraph-image`;
+
+  const breadcrumbJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      { '@type': 'ListItem', position: 1, name: 'Home', item: siteUrl },
+      { '@type': 'ListItem', position: 2, name: 'Blog', item: `${siteUrl}/blog` },
+      { '@type': 'ListItem', position: 3, name: post.title, item: pageUrl },
+    ],
+  };
+
+  const postJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    headline: post.title,
+    description: post.excerpt,
+    image: ogImage,
+    datePublished: post.date,
+    dateModified: post.date,
+    url: pageUrl,
+    keywords: post.tags?.join(', '),
+    author: { '@type': 'Person', name: siteConfig.fullName, url: siteUrl },
+  };
+
   return (
-    <BlogPostClient title={post.title} date={post.date} tags={post.tags}>
-      <MdxContent source={post.content} />
-    </BlogPostClient>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJsonLd) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(postJsonLd) }}
+      />
+      <BlogPostClient title={post.title} date={post.date} tags={post.tags}>
+        <MdxContent source={post.content} />
+      </BlogPostClient>
+    </>
   );
 }
